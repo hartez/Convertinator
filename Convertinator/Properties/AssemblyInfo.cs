2687b73622a71c4d8b891205c08d7b114bb7597b
E.Z. Hart <hartez@gmail.com>
2012-09-14 11:06:11 -0600

    Fixing package settings

--- Convertinator/Properties/AssemblyInfo.cs
+++ Convertinator/Properties/AssemblyInfo.cs
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4d0e07c5-2f6b-4386-93a9-dcf0cff6c0c9")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.2")]
 [assembly: AssemblyFileVersion("0.0.2")]
