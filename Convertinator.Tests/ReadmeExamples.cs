d174da1dbb95f13766aadc0a7980a556174bd9b1
E.Z. Hart <hartez@gmail.com>
2012-10-03 10:05:07 -0600

    Added currency example to unit tests

--- Convertinator.Tests/ReadmeExamples.cs
+++ Convertinator.Tests/ReadmeExamples.cs
@@ -60,5 +60,24 @@ namespace Convertinator.Tests
             Assert.That(feetMeasurement.ToAbbreviatedString() == "2 ft");
             Assert.That(feetMeasurement.ToString() == "2 feet");
         }
+
+        [Test]
+        public void Currencies()
+        {
+            var dollar = new Unit("dollar")
+                .UsePluralFormat("{0}s");
+
+            var yen = new Unit("yen");
+
+            var system = new ConversionGraph().RoundToDecimalPlaces(2);
+
+            system.AddConversion(Conversions.One(dollar).In(yen).Is(78.5300M));
+
+            var dollarAmount = new Measurement(dollar, 10);
+            var yenAmount = new Measurement(yen, 10000);
+
+            Assert.That(system.Convert(dollarAmount, yen) == 785.30M);
+            Assert.That(system.Convert(yenAmount, dollar) == 127.34M);
+        }
     }
 }
